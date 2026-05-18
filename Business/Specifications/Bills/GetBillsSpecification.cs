d9039e960afec61ce7d484e24ea953bbc28b0f98
imbatcat <ptdat04@gmail.com>
2026-05-18 19:05:26 +0700

    Update GetBillsSpecification.cs

--- Business/Specifications/Bills/GetBillsSpecification.cs
+++ Business/Specifications/Bills/GetBillsSpecification.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using Business.Handlers.Bills.GetBills.Dto;
 using Domain.Entities;
 
@@ -17,22 +18,32 @@ namespace Business.Specifications.Bills
                 && (!p.MaxTotal.HasValue || b.Total <= p.MaxTotal.Value))
         {
             var descending = p.SortOrder.Equals("desc", StringComparison.OrdinalIgnoreCase);
+            Action<Expression<Func<Bill, object>>> orderBy = descending ? AddOrderByDesc : AddOrderBy;
+            Action<Expression<Func<Bill, object>>> thenBy = descending ? AddThenByDesc : AddThenBy;
+            var sortFlag = true;
 
             switch (p.SortBy.ToLower())
             {
                 case "total":
-                    if (descending) AddOrderByDesc(b => b.Total!);
-                    else AddOrderBy(b => b.Total!);
+                    orderBy(b => b.Total!);
                     break;
                 case "merchantname":
-                    if (descending) AddOrderByDesc(b => b.MerchantName!);
-                    else AddOrderBy(b => b.MerchantName!);
+                    orderBy(b => b.MerchantName!);
                     break;
-                default: // "billdate"
-                    if (descending) AddOrderByDesc(b => b.BillDate);
-                    else AddOrderBy(b => b.BillDate);
+                default:
+                    sortFlag = false;
                     break;
             }
+
+            if (sortFlag)
+            {
+                thenBy(b => b.BillDate);
+            }
+            else
+            {
+                orderBy(b => b.BillDate);
+            }
+            thenBy(b => b.BillTime!);
 
             if (applyPaging)
                 AddPaging((p.Page - 1) * p.Size, p.Size);
